5d2eea32471e693ed0bab5d5b2efdba48902b474
misbahshakil233 <141862743+misbahshakil233@users.noreply.github.com>
2024-02-26 20:44:05 +0500

    Add files via upload

---
+++ nameCases.ts
@@ -0,0 +1,24 @@
+//ASSIGNMENT : 2
+console.log("ASSIGNMENT : 2")
+let peronalName:String="ERIC";
+console.log(`hello ${peronalName},would you like learn python today?`)
+
+//ASSIGNMENT : 3
+console.log("ASSIGNMENT : 3")
+let myname:string="Misbah";
+console.log(`lowercase:${myname.toLocaleLowerCase()}\n Uppercase ${myname.toUpperCase()}\n`)
+
+//ASSIGNMENT : 4
+console.log("ASSIGNMENT : 4")
+let Author:string="Allama Iqbal"
+let quotes:string="“Raise yourself so high that you reach every star, and if someone greets you, don’t greet them back.”"
+console.log(`${Author} once said, ${quotes}`)
+
+//ASSIGNMENT : 5
+console.log("ASSIGNMENT : 5")
+let Author2:string="Allama Iqbal"
+let quotes2:string="“Raise yourself so high that you reach every star, and if someone greets you, don’t greet them back.”"
+console.log(`${Author2} once said, ${quotes2}\n`)
+
+
+
